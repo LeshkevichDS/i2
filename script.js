d5f3d923c61651fd1989ac21a56e1c4e96d25fa3
LeshkevichDS <60656296+LeshkevichDS@users.noreply.github.com>
2020-08-28 16:25:57 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 var body = document.querySelector('body');
 var designs = document.querySelectorAll('.page-design');
 var theme = document.querySelector('.theme');
+var emails = document.querySelectorAll('.email');
+var phones = document.querySelectorAll('.phone');
 
 theme.onclick = function () {
     body.classList.toggle('dark');
     body.classList.toggle('light');
     for (let design of designs) {
-    design.classList.toggle('darkness');
-    design.classList.toggle('lightness');
-    }  
+        design.classList.toggle('dark1');
+        design.classList.toggle('light1');
+    }
+    for (let email of emails) {
+        email.classList.toggle('dark2');
+        email.classList.toggle('light2');
+    }
+    for (let phone of phones) {
+        phone.classList.toggle('dark3');
+        phone.classList.toggle('light3');
+    }
 };
 
