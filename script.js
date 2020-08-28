d925be657bf519ee68ec0653405fe9f51bce02c6
LeshkevichDS <60656296+LeshkevichDS@users.noreply.github.com>
2020-08-28 03:39:05 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 var body = document.querySelector('body');
-var design = document.querySelector('.page-design');
-var skills = document.querySelectorAll('.skills-item');
+var designs = document.querySelectorAll('.page-design');
 var theme = document.querySelector('.theme');
 
 theme.onclick = function () {
     body.classList.toggle('dark');
     body.classList.toggle('light');
+    for (let design of designs) {
     design.classList.toggle('darkness');
     design.classList.toggle('lightness');
-    for (let skill of skills) {
-    skill.classList.toggle('darkness');
-    skill.classList.toggle('lightness');
     }  
 };
 
